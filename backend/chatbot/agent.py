747e6e02bb952dcc17089c774c1bc82070a30261
Ali Haider <119880797+AIwithHaider@users.noreply.github.com>
2026-08-01 02:40:24 +0500

    add chat history to agent

--- backend/chatbot/agent.py
+++ backend/chatbot/agent.py
@@ -1,21 +1,28 @@
 from langchain.agents import create_agent
 from langchain_core.messages.ai import AIMessage
+from langgraph.checkpoint.memory import InMemorySaver
 
-from backend.chatbot.prompts import SYSTEM_PROMPT
 from backend.tools.search_tool import search_tool
 
+# Initialize the checkpointer once at the module level so memory persists across calls
+memory_saver = InMemorySaver()
 
-def create_chat_agent(llm, formatted_prompt, query):
+def create_chat_agent(llm, formatted_prompt, query, thread_id="default-session"):
 
     agent = create_agent(
         model=llm,
         tools=[search_tool],
-        system_prompt=formatted_prompt
+        system_prompt=formatted_prompt,
+        checkpointer=memory_saver # <-- Wires short-term memory into the graph loop
     )
 
     state = {"messages" : query}
-    response = agent.invoke(state)
-    # messages = response.get("messages")
+
+    # 4. Attach the thread configuration so the checkpointer targets the correct chat
+    config = {"configurable": {"thread_id": thread_id}}
+
+     # 5. Invoke the agent loop passing both state and config
+    response = agent.invoke(state, config=config)
     messages = response["messages"]
     ai_messages = [message.content for message in messages if isinstance(message, AIMessage)]
     return ai_messages[-1]
--- backend/chatbot/prompts.py
+++ backend/chatbot/prompts.py
@@ -1,12 +1,43 @@
-from langchain_core.prompts import ChatPromptTemplate
+# from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 
-SYSTEM_PROMPT = ChatPromptTemplate.from_template(
-    """
-    {system_prompt}.
-        Instructions:
-        - Be accurate.
-        - Be concise.
-        - Use the Tavily search tool whenever recent information is required.
-        - Never hallucinate.
+# SYSTEM_PROMPT = ChatPromptTemplate.from_template(
+#     """
+#     {system_prompt}.
+#         Instructions:
+#         - Be accurate.
+#         - Be concise.
+#         - Use the Tavily search tool whenever recent information is required.
+#         - Never hallucinate.
+# """
+# )
+
+# SYSTEM_PROMPT = ChatPromptTemplate.from_messages(
+#     [
+#         (
+#             "system",
+#             """
+#                 {system_prompt}.
+#                     Instructions:
+#                     - Be accurate.
+#                     - Be concise.
+#                     - Use the Tavily search tool whenever recent information is required.
+#                     - Never hallucinate.
+#             """
+#         ),
+
+#         MessagesPlaceholder(variable_name="history"),
+
+
+#     ]
+# )
+
+
+
+SYSTEM_PROMPT = """
+{system_prompt}.
+Instructions:
+- Be accurate.
+- Be concise.
+- Use the Tavily search tool whenever recent information is required.
+- Never hallucinate.
 """
-)
