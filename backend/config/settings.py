d41a3beb14d25e1a9213f4b53d85cdf7c97b44c4
Ali Haider <119880797+AIwithHaider@users.noreply.github.com>
2026-08-20 03:53:19 +0500

    fix error occured by last changes

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -5,22 +5,17 @@ from typing import Optional
 class Settings(BaseSettings):
     # Groq
     groq_api_key: str
-    groq_model_name: str
     
     
     # Gemini
     gemini_api_key: str
-    gemini_model_name: str
 
     # Mistral
     mistral_api_key: str
-    mistral_model_name: str
 
     temperature: float
+    
     tavily_api_key: str
-    glm_api_key: Optional[str] = None
-    openai_api_key: Optional[str] = None
-
 
     model_config = SettingsConfigDict(
         env_file= ".env",
